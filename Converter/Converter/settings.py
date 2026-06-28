f193484222572aa5d8cd0287ad33af3d7cec3eae
natiknn <natiknn02@gmail.com>
2026-06-28 10:22:24 +0300

    fixed settings

--- Converter/Converter/settings.py
+++ Converter/Converter/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # Загружаем .env из той же папки
 load_dotenv(BASE_DIR / '.env')
-SECRET_KEY = os.getenv('SECRET_KEY')
-print(f"DEBUG: SECRET_KEY = {SECRET_KEY}")
+
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+print(f"DEBUG: SECRET_KEY = {SECRET_KEY}")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
